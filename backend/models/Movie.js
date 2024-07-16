e4ead16f88cfce705a8f50997a7d556b75cc8542
Alban Danet <60749798+albiche@users.noreply.github.com>
2024-07-16 08:51:15 +0200

    push

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   genre: { type: String, required: true },
-  year: { type: Number, required: true }
+  year: { type: Number, required: true },
+  image: { type: String } // Ajouter le champ image
 });
 
 module.exports = mongoose.model('Movie', movieSchema);
