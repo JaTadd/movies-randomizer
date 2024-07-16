a4f81836e9fee067032ee2931388a50e9c81e31f
JaTadd <jean-alexis.taddei@efrei.net>
2024-07-16 12:48:06 +0200

    websocket chat

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRoutes = require('./routes/user');
-const movieRoutes = require('./routes/movie');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/user");
+const movieRoutes = require("./routes/movie");
+// Socket
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(express.json());
 app.use(cors());
 
 // Connexion à la base de données MongoDB
-mongoose.connect('mongodb://localhost:27017/movieDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost:27017/movieDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Utilisation des routes
-app.use('/api/users', userRoutes);
-app.use('/api/movies', movieRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/movies", movieRoutes);
+
+// Stockage de l'historique des messages
+let messageHistory = [];
+
+// Gestion connexion socket
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  // Envoyer l'historique des messages au client qui vient de se connecter
+  socket.emit("messageHistory", messageHistory);
+
+  socket.on("sendMessage", (data) => {
+    // Ajouter le message à l'historique
+    messageHistory.push(data);
+    io.emit("newMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
 
 // Démarrage du serveur
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Profile from './pages/Profile';
-import SearchMovies from './pages/SearchMovies';
-import AdminPage from './pages/AdminPage';
-import Header from './components/Header';  // Importer le Header
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import SearchMovies from "./pages/SearchMovies";
+import AdminPage from "./pages/AdminPage";
+import Header from "./components/Header";
+import ChatSlider from "./components/ChatSlider";
 
 function App() {
   return (
     <Router>
-      <Header />  {/* Inclure le Header */}
+      <Header /> {/* Inclure le Header */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/search" element={<SearchMovies />} />
-        <Route path="/admin" element={<AdminPage />} />  
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
+      <ChatSlider />
     </Router>
   );
 }
