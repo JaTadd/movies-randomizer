005a64b5e5c329063965baffa4fe26d567af862a
JaTadd <jean-alexis.taddei@efrei.net>
2024-07-21 19:39:07 +0200

    chat css

--- frontend/src/components/ChatRoom.js
+++ frontend/src/components/ChatRoom.js
@@ -1,15 +1,16 @@
 // src/components/ChatRoom.js
 import React, { useState } from "react";
+import './ChatRoom.css'; 
 
 function ChatRoom({ messages, setMessages, socket }) {
   const [newMessage, setNewMessage] = useState("");
-  const username = localStorage.getItem("username"); // Récupérer le username
-  const isAuthenticated = !!username; // Vérifier si l'utilisateur est authentifié
+  const username = localStorage.getItem("username"); 
+  const isAuthenticated = !!username; 
 
   const handleSendMessage = () => {
     if (newMessage.trim() && isAuthenticated) {
       const message = {
-        user: username, // Utiliser le pseudo de l'utilisateur
+        user: username, 
         text: newMessage,
         timestamp: new Date().toISOString(),
       };
@@ -19,30 +20,15 @@ function ChatRoom({ messages, setMessages, socket }) {
   };
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100%" }}>
-      <div
-        style={{
-          height: "calc(100% - 60px)",
-          overflowY: "auto",
-          padding: "10px",
-          border: "1px solid gray",
-        }}
-      >
+    <div className="chat-room">
+      <div className="chat-messages">
         {messages.map((msg, index) => (
-          <div key={index} style={{ margin: "10px 0" }}>
+          <div key={index} className="chat-message">
             <strong>{msg.user}:</strong> {msg.text}
           </div>
         ))}
       </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          padding: "10px",
-          backgroundColor: "white",
-        }}
-      >
+      <div className="chat-input-container">
         <input
           type="text"
           value={newMessage}
@@ -52,7 +38,7 @@ function ChatRoom({ messages, setMessages, socket }) {
               handleSendMessage();
             }
           }}
-          style={{ width: "80%" }}
+          className="chat-input"
           disabled={!isAuthenticated} // Désactivation de l'input si l'utilisateur n'est pas connecté
           placeholder={
             isAuthenticated ? "Type a message" : "Please log in to chat"
@@ -60,7 +46,7 @@ function ChatRoom({ messages, setMessages, socket }) {
         />
         <button
           onClick={handleSendMessage}
-          style={{ width: "20%" }}
+          className="chat-send-button"
           disabled={!isAuthenticated}
         >
           Send
